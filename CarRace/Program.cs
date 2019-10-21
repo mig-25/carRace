fafbfa13316db50cced77a1b7a977d791654d08e
mig-25 <kd3180@hotmail.com>
2019-10-21 23:03:58 +0200

    Add project files.

---
+++ CarRace/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CarRace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
